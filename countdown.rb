7f5b7a063ba5b4efc7c087df819710a3ede6d4a0
Blake Stoner <stonerblake@gmail.com>
2019-04-14 19:22:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,9 +10,9 @@ end
 end
 
 def countdown_with_sleep(seconds)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(5)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
     seconds -= 1
   end
   "HAPPY NEW YEAR!"
@@ -20,23 +20,3 @@ end
 
 
 
-
-
-
-
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    sleep(1)
-    seconds_to_midnight -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
